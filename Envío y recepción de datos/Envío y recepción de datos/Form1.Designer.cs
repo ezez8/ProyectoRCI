b57d1b733fd66714c761fdb4f058b2f9f55de8ed
thonygrz <anthony81199@gmail.com>
2019-06-02 16:17:11 -0400

    envío y recepción de datos, simulación de pedir envido

---
+++ Envío y recepción de datos/Envío y recepción de datos/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Envío_y_recepción_de_datos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BtnBuscarPuertos = new System.Windows.Forms.Button();
+            this.BtnConectar = new System.Windows.Forms.Button();
+            this.BtnEnviarDatos = new System.Windows.Forms.Button();
+            this.LblBaudRate = new System.Windows.Forms.Label();
+            this.CboPuertos = new System.Windows.Forms.ComboBox();
+            this.CboBaudRate = new System.Windows.Forms.ComboBox();
+            this.LblDatosRecibidos = new System.Windows.Forms.Label();
+            this.TxtDatosEnviados = new System.Windows.Forms.TextBox();
+            this.TxtDatosRecibidos = new System.Windows.Forms.TextBox();
+            this.SpPuertos = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // BtnBuscarPuertos
+            // 
+            this.BtnBuscarPuertos.Location = new System.Drawing.Point(27, 56);
+            this.BtnBuscarPuertos.Name = "BtnBuscarPuertos";
+            this.BtnBuscarPuertos.Size = new System.Drawing.Size(101, 23);
+            this.BtnBuscarPuertos.TabIndex = 0;
+            this.BtnBuscarPuertos.Text = "Buscar Puertos";
+            this.BtnBuscarPuertos.UseVisualStyleBackColor = true;
+            this.BtnBuscarPuertos.Click += new System.EventHandler(this.BtnBuscarPuertos_Click);
+            // 
+            // BtnConectar
+            // 
+            this.BtnConectar.Location = new System.Drawing.Point(336, 54);
+            this.BtnConectar.Name = "BtnConectar";
+            this.BtnConectar.Size = new System.Drawing.Size(218, 48);
+            this.BtnConectar.TabIndex = 1;
+            this.BtnConectar.Text = "CONECTAR";
+            this.BtnConectar.UseVisualStyleBackColor = true;
+            this.BtnConectar.Click += new System.EventHandler(this.BtnConectar_Click);
+            // 
+            // BtnEnviarDatos
+            // 
+            this.BtnEnviarDatos.Location = new System.Drawing.Point(27, 248);
+            this.BtnEnviarDatos.Name = "BtnEnviarDatos";
+            this.BtnEnviarDatos.Size = new System.Drawing.Size(101, 23);
+            this.BtnEnviarDatos.TabIndex = 2;
+            this.BtnEnviarDatos.Text = "Enviar Datos";
+            this.BtnEnviarDatos.UseVisualStyleBackColor = true;
+            this.BtnEnviarDatos.Click += new System.EventHandler(this.BtnEnviarDatos_Click);
+            // 
+            // LblBaudRate
+            // 
+            this.LblBaudRate.AutoSize = true;
+            this.LblBaudRate.Location = new System.Drawing.Point(24, 166);
+            this.LblBaudRate.Name = "LblBaudRate";
+            this.LblBaudRate.Size = new System.Drawing.Size(55, 13);
+            this.LblBaudRate.TabIndex = 4;
+            this.LblBaudRate.Text = "BaudRate";
+            // 
+            // CboPuertos
+            // 
+            this.CboPuertos.FormattingEnabled = true;
+            this.CboPuertos.Location = new System.Drawing.Point(157, 56);
+            this.CboPuertos.Name = "CboPuertos";
+            this.CboPuertos.Size = new System.Drawing.Size(121, 21);
+            this.CboPuertos.TabIndex = 5;
+            this.CboPuertos.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // CboBaudRate
+            // 
+            this.CboBaudRate.FormattingEnabled = true;
+            this.CboBaudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "56000",
+            "115200"});
+            this.CboBaudRate.Location = new System.Drawing.Point(157, 158);
+            this.CboBaudRate.Name = "CboBaudRate";
+            this.CboBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.CboBaudRate.TabIndex = 6;
+            this.CboBaudRate.Text = "9600";
+            // 
+            // LblDatosRecibidos
+            // 
+            this.LblDatosRecibidos.AutoSize = true;
+            this.LblDatosRecibidos.Location = new System.Drawing.Point(24, 330);
+            this.LblDatosRecibidos.Name = "LblDatosRecibidos";
+            this.LblDatosRecibidos.Size = new System.Drawing.Size(85, 13);
+            this.LblDatosRecibidos.TabIndex = 7;
+            this.LblDatosRecibidos.Text = "Datos Recibidos";
+            this.LblDatosRecibidos.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TxtDatosEnviados
+            // 
+            this.TxtDatosEnviados.Location = new System.Drawing.Point(157, 248);
+            this.TxtDatosEnviados.Name = "TxtDatosEnviados";
+            this.TxtDatosEnviados.Size = new System.Drawing.Size(397, 20);
+            this.TxtDatosEnviados.TabIndex = 8;
+            // 
+            // TxtDatosRecibidos
+            // 
+            this.TxtDatosRecibidos.Location = new System.Drawing.Point(157, 323);
+            this.TxtDatosRecibidos.Name = "TxtDatosRecibidos";
+            this.TxtDatosRecibidos.Size = new System.Drawing.Size(397, 20);
+            this.TxtDatosRecibidos.TabIndex = 9;
+            // 
+            // SpPuertos
+            // 
+            this.SpPuertos.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.DatoRecibido);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 385);
+            this.Controls.Add(this.TxtDatosRecibidos);
+            this.Controls.Add(this.TxtDatosEnviados);
+            this.Controls.Add(this.LblDatosRecibidos);
+            this.Controls.Add(this.CboBaudRate);
+            this.Controls.Add(this.CboPuertos);
+            this.Controls.Add(this.LblBaudRate);
+            this.Controls.Add(this.BtnEnviarDatos);
+            this.Controls.Add(this.BtnConectar);
+            this.Controls.Add(this.BtnBuscarPuertos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBuscarPuertos;
+        private System.Windows.Forms.Button BtnConectar;
+        private System.Windows.Forms.Button BtnEnviarDatos;
+        private System.Windows.Forms.Label LblBaudRate;
+        private System.Windows.Forms.ComboBox CboPuertos;
+        private System.Windows.Forms.ComboBox CboBaudRate;
+        private System.Windows.Forms.Label LblDatosRecibidos;
+        private System.Windows.Forms.TextBox TxtDatosEnviados;
+        private System.Windows.Forms.TextBox TxtDatosRecibidos;
+        private System.IO.Ports.SerialPort SpPuertos;
+    }
+}
+
---
+++ Envío y recepción de datos/Envío y recepción de datos/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+
+namespace Envío_y_recepción_de_datos
+{
+    public partial class Form1 : Form
+    {
+        //------------DELEGADO ???-------------------
+        private delegate void DelegadoAcceso(string accion);
+        //------------Strings donde se enviarán y recibirán los datos----------
+        private string strBufferIn;
+        private string strBufferOut;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AccesoForm(string accion)
+        {
+            strBufferIn = accion;
+            //---------AQUÍ SE MANEJA EL MENSAJE RECIBIDO-------------
+            TxtDatosRecibidos.Text = strBufferIn;
+            //--------SIMULACIÓN DE PEDIR ENVIDO---------------
+            if(strBufferIn[7] == 'S')
+            {
+                EnviarDatos("$$STSG#S######%%");
+            }
+            //--------------------------------------------------------
+        }
+
+        //------------DELEGADO ???-------------------
+        private void AccesoInterrupcion(string accion)
+        {
+            DelegadoAcceso Var_DelegadoAcceso;
+            Var_DelegadoAcceso = new DelegadoAcceso(AccesoForm);
+            object[] arg = { accion };
+            base.Invoke(Var_DelegadoAcceso, arg);
+        }
+
+        //-----Se inicializan los buffers y se desabilitan los botones 'CONECTAR' y 'ENVIAR DATOS'
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            strBufferIn = "";
+            strBufferOut = "";
+            BtnConectar.Enabled = false;
+            BtnEnviarDatos.Enabled = false;
+        }
+
+        
+        private void BtnBuscarPuertos_Click(object sender, EventArgs e)
+        {
+            //Se buscan los puertos disponibles de la computadora
+            string[] PuertosDisponibles = SerialPort.GetPortNames();
+
+            //Se limpia el comboBox
+            CboPuertos.Items.Clear();
+
+            //Se llena el comboBox con los puertos disponibles
+            foreach (string puerto in PuertosDisponibles)
+            {
+                CboPuertos.Items.Add(puerto);
+            }
+
+            //Si hay puertos disponibles se habilita 'CONECTAR' si no se avisa que no hay puertos disponibles
+            if(CboPuertos.Items.Count > 0)
+            {
+                CboPuertos.SelectedIndex = 0;
+                MessageBox.Show("SELECCIONA UN PUERTO");
+                BtnConectar.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("NO SE DETECTARON PUERTOS DISPONIBLES");
+                CboPuertos.Items.Clear();
+                CboPuertos.Text = "                     ";
+                strBufferIn = "";
+                strBufferOut = "";
+                BtnConectar.Enabled = false;
+                BtnEnviarDatos.Enabled = false;
+            }
+        }
+
+        private void BtnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //'CONECTAR' configura el puerto, lo abre, cambia a 'DESCONECTAR' y habilita el botón 'Enviar Datos'
+                if(BtnConectar.Text == "CONECTAR")
+                {
+                    SpPuertos.BaudRate = Int32.Parse(CboBaudRate.Text);
+                    SpPuertos.DataBits = 8;
+                    SpPuertos.Parity = Parity.None;
+                    SpPuertos.StopBits = StopBits.One;
+                    SpPuertos.Handshake = Handshake.None;
+                    SpPuertos.PortName = CboPuertos.Text;
+
+                    try
+                    {
+                        SpPuertos.Open();
+                        BtnConectar.Text = "DESCONECTAR";
+                        BtnEnviarDatos.Enabled = true;
+                    }
+                    catch(Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                //'DESCONECTAR' cierra el puerto, cambia a 'CONECTAR' y desabilita el botón 'Enviar Datos'
+                else if(BtnConectar.Text == "DESCONECTAR")
+                {
+                    SpPuertos.Close();
+                    BtnConectar.Text = "CONECTAR";
+                    BtnEnviarDatos.Enabled = false;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                //Si hay algún problema conectándose se muestra por pantalla el error
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void BtnEnviarDatos_Click(object sender, EventArgs e)
+        {
+            //Se llena el buffer de salida y se envían los datos
+            try
+            {
+                //---------AQUÍ SE MANEJA EL MENSAJE ENVIADO-------------
+                SpPuertos.DiscardOutBuffer();
+                strBufferOut = TxtDatosEnviados.Text;
+                SpPuertos.Write(strBufferOut);
+                //-------------------------------------------------------
+            }
+            catch (Exception ex)
+            {
+                //Si hay algún problema enviando datos se muestra por pantalla el error
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void EnviarDatos(string msg)
+        {
+            //Se llena el buffer de salida y se envían los datos
+            try
+            {
+                //---------AQUÍ SE MANEJA EL MENSAJE ENVIADO-------------
+                SpPuertos.DiscardOutBuffer();
+                strBufferOut = msg;
+                SpPuertos.Write(msg);
+                //-------------------------------------------------------
+            }
+            catch (Exception ex)
+            {
+                //Si hay algún problema enviando datos se muestra por pantalla el error
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void DatoRecibido(object sender, SerialDataReceivedEventArgs e)
+        {
+            //----Se recibe el mensaje a través de DELEGADOS ???--------------
+            AccesoInterrupcion(SpPuertos.ReadExisting());
+
+            /*string Data_in = SpPuertos.ReadExisting();
+            MessageBox.Show(Data_in);
+            TxtDatosRecibidos.Text = Data_in;*/
+        }
+
+        //------------NO HACE NADA PERO NO SE PUEDE QUITAR
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}

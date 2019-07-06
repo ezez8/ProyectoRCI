151bad0090d431a1d33fb13adae11919b2751c05
reinel215 <44073967+reinel215@users.noreply.github.com>
2019-07-06 14:41:52 -0400

    conexión entre puertos
    se creo la conexión entre puertos seriales llamados "puerto salida" y "puerto entrada" 
    para utilizarlos conectar a los puertos virtuales y listo.

---
+++ cambio turnos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace turnos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            puerto sp = new puerto();
+            sp.turnoA();
+            sp.turnoB();
+            sp.turnoC();
+            sp.turnoD();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ cambio turnos/puerto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace turnos
+{
+    public class puerto
+    {
+        private SerialPort puertoSalida = new SerialPort();
+        private SerialPort puertoEntrada = new SerialPort();
+
+        public puerto()
+        {
+            Console.WriteLine("el puerto ha sido creado");
+
+            puertoSalida.BaudRate = 9600;
+            puertoSalida.DataBits = 8;
+            puertoSalida.Parity = Parity.None;
+            puertoSalida.StopBits = StopBits.One;
+            puertoSalida.Handshake = Handshake.None;
+            puertoSalida.PortName = "COM2";
+
+
+            puertoEntrada.BaudRate = 9600;
+            puertoEntrada.DataBits = 8;
+            puertoEntrada.Parity = Parity.None;
+            puertoEntrada.StopBits = StopBits.One;
+            puertoEntrada.Handshake = Handshake.None;
+            puertoEntrada.PortName = "COM3";
+
+
+
+            puertoEntrada.DataReceived += new SerialDataReceivedEventHandler(llegaronDatos); //coloco la interrupcion de entrada de datos
+
+
+            puertoSalida.Open();
+            puertoEntrada.Open();
+
+            puertoEntrada.ReadTimeout = 500;
+        }
+
+
+
+
+
+        private static void llegaronDatos(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.WriteLine("Data Received:");
+            Console.Write(indata);
+        }
+
+
+
+        public void turnoA()
+        {
+            puertoSalida.WriteLine("$$SASGA#######%%");
+        }
+
+        public void turnoB()
+        {
+            puertoSalida.WriteLine("$$SBSGB#######%%");
+        }
+
+        public void turnoC()
+        {
+            puertoSalida.WriteLine("$$SCSGC#######%%");
+        }
+
+        public void turnoD()
+        {
+            puertoSalida.WriteLine("$$SDSGD#######%%");
+        }
+
+    }
+
+}

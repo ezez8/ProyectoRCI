aecc7c14108510a21e192a2d11d2a35b64514be8
ezez8 <ezez8@users.noreply.github.com>
2019-07-07 11:25:42 -0400

    arregle carta para la trama

--- Truco/TrucoHost/TrucoHost/Clases/Mazo.cs
+++ Truco/TrucoHost/TrucoHost/Clases/Mazo.cs
@@ -21,9 +21,50 @@ namespace TrucoHost.Clases
         {
             cartas.Clear();
 
-            foreach (Palo palo in Enum.GetValues(typeof(Palo)))
-                foreach (Numeracion numeracion in Enum.GetValues(typeof(Numeracion)))
-                    cartas.Add(new Carta(numeracion, palo));
+            cartas.Add(new Carta("O", "#1"));
+            cartas.Add(new Carta("O", "#2"));
+            cartas.Add(new Carta("O", "#3"));
+            cartas.Add(new Carta("O", "#4"));
+            cartas.Add(new Carta("O", "#5"));
+            cartas.Add(new Carta("O", "#6"));
+            cartas.Add(new Carta("O", "#7"));
+            cartas.Add(new Carta("O", "10"));
+            cartas.Add(new Carta("O", "11"));
+            cartas.Add(new Carta("O", "12"));
+
+            cartas.Add(new Carta("C", "#1"));
+            cartas.Add(new Carta("C", "#2"));
+            cartas.Add(new Carta("C", "#3"));
+            cartas.Add(new Carta("C", "#4"));
+            cartas.Add(new Carta("C", "#5"));
+            cartas.Add(new Carta("C", "#6"));
+            cartas.Add(new Carta("C", "#7"));
+            cartas.Add(new Carta("C", "10"));
+            cartas.Add(new Carta("C", "11"));
+            cartas.Add(new Carta("C", "12"));
+
+            cartas.Add(new Carta("E", "#1"));
+            cartas.Add(new Carta("E", "#2"));
+            cartas.Add(new Carta("E", "#3"));
+            cartas.Add(new Carta("E", "#4"));
+            cartas.Add(new Carta("E", "#5"));
+            cartas.Add(new Carta("E", "#6"));
+            cartas.Add(new Carta("E", "#7"));
+            cartas.Add(new Carta("E", "10"));
+            cartas.Add(new Carta("E", "11"));
+            cartas.Add(new Carta("E", "12"));
+
+            cartas.Add(new Carta("B", "#1"));
+            cartas.Add(new Carta("B", "#2"));
+            cartas.Add(new Carta("B", "#3"));
+            cartas.Add(new Carta("B", "#4"));
+            cartas.Add(new Carta("B", "#5"));
+            cartas.Add(new Carta("B", "#6"));
+            cartas.Add(new Carta("B", "#7"));
+            cartas.Add(new Carta("B", "10"));
+            cartas.Add(new Carta("B", "11"));
+            cartas.Add(new Carta("B", "12"));
+
         }
 
         public Carta getCarta()

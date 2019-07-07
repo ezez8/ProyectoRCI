9b475d98ea5749c047b903553d8b555e2f807fd8
ezez8 <ezez8@users.noreply.github.com>
2019-07-07 15:47:51 -0400

    determina ganador

--- Truco/TrucoHost/TrucoHost/Clases/Carta.cs
+++ Truco/TrucoHost/TrucoHost/Clases/Carta.cs
@@ -17,6 +17,52 @@ namespace TrucoHost.Clases
             this.palo = palo;
         }
 
+        public int valor(Carta vira)
+        {
+            if (palo == vira.palo) {
+                if (vira.num == "10" && num == "12")
+                    return 14;
+                if (vira.num == "11" && num == "12")
+                    return 15;
+                if (vira.num != "10" && num == "10")
+                    return 14;
+                if (vira.num != "11" && num == "11")
+                    return 15;
+            }
+
+
+            if (num == "#4")
+                return 0;
+            else if (num == "#5")
+                return 1;
+            else if (num == "#6")
+                return 2;
+            else if (num == "#7" && (palo == "C" || palo == "B"))
+                return 3;
+            else if (num == "10")
+                return 4;
+            else if (num == "11")
+                return 5;
+            else if (num == "12")
+                return 6;
+            else if (num == "#1" && (palo == "C" || palo == "O"))
+                return 7;
+            else if (num == "#2")
+                return 8;
+            else if (num == "#3")
+                return 9;
+            else if (num == "#7" && palo == "O")
+                return 10;
+            else if (num == "#7" && palo == "E")
+                return 11;
+            else if (num == "#1" && palo == "B")
+                return 12;
+            else if (num == "#1" && palo == "E")
+                return 13;
+            else
+                return -1;              
+        }
+
         public string toString()
         {
             return palo + " | " + num;
--- Truco/TrucoHost/TrucoHost/Clases/Jugador.cs
+++ Truco/TrucoHost/TrucoHost/Clases/Jugador.cs
@@ -34,35 +34,45 @@ namespace TrucoHost.Clases
 
         public Carta jugarCarta()
         {
-            Console.WriteLine("Selecciona una carta jugador " + id);
-            if (a != null)
-                Console.WriteLine("1. " + a.toString());
-            if (b != null)
-                Console.WriteLine("2. " + b.toString());
-            if (c != null)
-                Console.WriteLine("3. " + c.toString());
-            Console.WriteLine(">> ");
-
             Carta auxc;
+            do {
 
-            switch (Console.ReadLine())
-            {
-                case "1":
-                    auxc = a;
-                    a = null;
-                    break;
-                case "2":
-                    auxc = b;
-                    b = null;
-                    break;
-                case "3":
-                    auxc = c;
-                    c = null;
-                    break;
-                default:
-                    auxc = null;
-                    break;
-            }
+                Console.WriteLine("SELECCIONA UNA CARTA JUGADOR " + id);
+                if (a != null)
+                    Console.WriteLine("1. " + a.toString());
+                if (b != null)
+                    Console.WriteLine("2. " + b.toString());
+                if (c != null)
+                    Console.WriteLine("3. " + c.toString());
+                Console.Write(">> ");
+                          
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        auxc = a;
+                        a = null;
+                        break;
+                    case "2":
+                        auxc = b;
+                        b = null;
+                        break;
+                    case "3":
+                        auxc = c;
+                        c = null;
+                        break;
+                    default:
+                        auxc = null;
+                        break;
+                }
+
+                if (auxc == null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("<<ERROR DEBE SELECCIONAR UNA CARTA!!!>>");
+                    Console.WriteLine();
+                }
+
+            } while (auxc == null);
 
             return auxc;
         }
